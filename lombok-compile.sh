4e86f0e6920ec23b00f038d7e68cea9fa93f3fad
Miguel García-López <miguel.garcia@displaynote.com>
2015-03-15 22:53:16 +0100

    Trying to call j2objc with lombok as javaagent.

--- lombok-compile.sh
+++ lombok-compile.sh
@@ -2,9 +2,10 @@
 
 M2_REPO=~/.m2/repository
 
-LOMBOK_DEP=/org/projectlombok/lombok/1.16.2/lombok-1.16.2.jar
+LOMBOK_DEP=org/projectlombok/lombok/1.16.2/lombok-1.16.2.jar
 
-j2objc -classpath ${M2_REPO}/${LOMBOK_DEP} \
+j2objc.lombok -begin-java-args -javaagent:${M2_REPO}/${LOMBOK_DEP} -end-java-args \
+       -classpath ${M2_REPO}/${LOMBOK_DEP} \
        -d target/j2objc-generated \
        src/main/java/com/mgl/j2objctests/LombokTest.java
 
